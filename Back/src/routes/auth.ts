c5381bb84c03d185074c42647f94da3f560576b0
juanoned <juandiego.jd42@gmail.com>
2025-02-27 17:13:32 -0300

    Make models on prisma

--- Back/src/routes/auth.ts
+++ Back/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import AuthController from '../controllers/AuthController.ts'
+import { validadeLogin, validadeRegister } from '../middlewares/authMiddleware.ts'
 
 export default express.Router()
-    .post('/register',AuthController.register)
-    .post('/login',AuthController.login)
+    .post('/register',validadeRegister,AuthController.register)
+    .post('/login',validadeLogin,AuthController.login)
